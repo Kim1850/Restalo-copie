54ca3104cbe7a6c09ce4795532f68bade0eb55a5
Kim St-Pierre <kistp2@ulaval.ca>
2024-02-19 14:56:36 -0500

    feat: add exception mappers

---
+++ src/main/java/ca/ulaval/glo2003/exceptions/NullPointerExceptionMapper.java
@@ -0,0 +1,14 @@
+package ca.ulaval.glo2003.exceptions;
+
+import ca.ulaval.glo2003.api.responses.ErrorResponse;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+public class NullPointerExceptionMapper implements ExceptionMapper<NullPointerException> {
+    @Override
+    public Response toResponse(NullPointerException e) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(new ErrorResponse("MISSING_PARAMETER", e.getMessage()))
+                .build();
+    }
+}
